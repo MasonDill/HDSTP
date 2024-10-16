b259485d1e6fce8883ad6fd8926afbc53444904c
Mason Dill <mdill@cervis.net>
2024-10-16 13:52:26 -0400

    added PacketStream

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::time::{Duration, Instant};
+use std::collections::VecDeque;
+use tokio::sync::Notify;
 
 pub enum PacketType {
     // Data packets (0b0XXX)
@@ -25,13 +27,43 @@ pub struct Packet {
 impl Packet {
     fn new(packet_type: PacketType, sequence_no: u32, data: Vec<u8>) -> Self {
         let length = (data.len() + 9) as u32; // 4 bytes for length + 1 byte for type + 4 bytes for sequence no + parity
-        let parity = Self::check_parity(&data);
+        let parity = check_parity(&data);
         Self { length, packet_type, sequence_no, data, parity }
     }
+}
 
-    // Function to calculate parity (even parity)
-    fn check_parity(data: &[u8]) -> u8 {
-        let ones_count = data.iter().map(|&byte| byte.count_ones()).sum::<u32>() % 2;
-        if ones_count == 0 { 0b0000 } else { 0b1111 }
+// Function to calculate parity (even parity)
+fn check_parity(data: &[u8]) -> u8 {
+    let ones_count = data.iter().map(|&byte| byte.count_ones()).sum::<u32>() % 2;
+    if ones_count == 0 { 0b0000 } else { 0b1111 }
+}
+
+pub struct PacketStream {
+    buffer: VecDeque<Packet>,
+    notify: Notify,
+}
+
+impl PacketStream {
+    pub fn new() -> Self {
+        PacketStream {
+            buffer: VecDeque::new(),
+            notify: Notify::new(),
+        }
+    }
+
+    pub fn write(&mut self, packet: Packet) {
+        self.buffer.push_back(packet);
+        self.notify.notify_one();
+    }
+
+    pub async fn read(&mut self) -> Option<Packet> {
+        self.notify.notified().await;
+        let packet = if self.buffer.is_empty() {
+            None
+        } else {
+            Some(self.buffer.pop_front().unwrap())
+        };
+
+        packet
     }
 }
